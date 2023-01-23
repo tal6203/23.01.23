a4f1eb84acfc876ac9c203409c219828a8895a34
tal6203 <tal6203@gmail.com>
2023-01-23 21:10:17 +0200

    upload

---
+++ src/FirstProject.java
@@ -0,0 +1,5 @@
+public class FirstProject {
+   public static void FirstProject(){
+        System.out.println("Tal abutbul");
+    }
+}
---
+++ src/PrimitiveVariable.java
@@ -0,0 +1,8 @@
+public class PrimitiveVariable {
+    static void PrimitveVar(){
+        double b = 25.23;
+        int my_int1 = (int) b;
+        System.out.println(my_int1);
+    }
+
+}
