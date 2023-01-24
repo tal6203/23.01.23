4c9e3f65737660c4472d4a653a4466b4b0fb07c1
tal6203 <tal6203@gmail.com>
2023-01-24 10:17:47 +0200

    upload

--- src/PrimitiveVariable.java
+++ src/PrimitiveVariable.java
@@ -1,8 +1,13 @@
 public class PrimitiveVariable {
     static void PrimitveVar(){
-        double b = 25.23;
-        int my_int1 = (int) b;
-        System.out.println(my_int1);
+        double a = 25.23;
+        int b = 7;
+        double c = 6.32;
+//        int num3 = int c;
+        double num1 =  b;
+        int num2 = (int) a;
+        System.out.println(num2);
+        System.out.println(num1);
     }
 
 }
